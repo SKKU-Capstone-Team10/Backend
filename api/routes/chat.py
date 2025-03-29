3ef0354ffef29b505dc247757f010e77138c03e4
ljh4770 <ljh4770@naver.com>
2025-03-29 21:17:47 +0900

    feat(chat): Init route for creating chat.
    POST /api/chat/

--- api/routes/chat.py
+++ api/routes/chat.py
@@ -1,17 +1,26 @@
+from typing import Any
+
 from fastapi import APIRouter, HTTPException
 
-from models import (
-    Chat
+from core.db import SessionDep
+
+from models import Chat
+from schemas.chat import (
+    ChatCreate,
+    ChatHistoryResponse,
+    ChatResponse
 )
 
 router = APIRouter(prefix="/chat", tags=['Chat'])
 
 # Initial chat -> Create a new Chat Session
-@router.post('/initial')
-def init_chat():
-    return "tmp"
-
-# New Chat
-@router.post('/')
-def upload_chat():
-    return "tmp"
+@router.post('/', response_model=ChatResponse)
+def create_chat(db: SessionDep, req: ChatCreate) -> Any:
+    # Pass chat content to AI server
+    if req.session_id == None:
+        # Create a new chat session
+        pass
+    else:
+        # Add requested content to the chat session
+        pass
+    # Response the message from AI server
