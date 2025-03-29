292ab57addbfa8dc88d0ffa12e91b184f795f657
ljh4770 <ljh4770@naver.com>
2025-03-29 22:55:33 +0900

    feat(chat_session): init basic crud operations. No implementation

--- crud/chat_session.py
+++ crud/chat_session.py
@@ -1,5 +1,17 @@
 from uuid import UUID
+from typing import List
 from sqlmodel import Session
 
-def create_session(db: Session, user_id: UUID) -> UUID:
+from models import ChatSession
+
+def create_session(db: Session, user_id: UUID) -> ChatSession:
+    pass
+
+def read_sessions(db: Session, user_id: UUID) -> List[ChatSession]:
+    pass
+
+def update_session_title(db: Session, session_id: UUID, new_title: str) -> None:
+    pass
+
+def delete_session(db: Session, session_id: UUID) -> None:
     pass
