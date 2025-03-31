c15991ea85c2cab9a5b2855018dbfec9b6ead99d
ljh4770 <ljh4770@naver.com>
2025-03-31 10:56:29 +0900

    docs(chat): Add comment for swagger documentation form """ description - param: parameter description 000 Error - HTTP Exception description """

--- api/routes/chat.py
+++ api/routes/chat.py
@@ -26,6 +26,15 @@ executor = ThreadPoolExecutor()
 # Initial chat -> Create a new Chat Session
 @router.post('/', response_model=ChatResponse)
 def post_chat(db: SessionDep, req: ChatCreate, current_user: CurrentUser) -> Any:
+    """
+    Add new chat from user and get AI's response message. \n
+    Token Required \n
+    - **session_id** Optional: Chat Session to add the chats both from user and AI. Remain **None** to create new chat session.
+    - **sender**: Enum type[user/host], Request it with 'user', Response filled with 'host'
+    - **content**: chat content from user \n
+    403 Error - Invalid token. \n
+    404 Error - User with the token not found.
+    """
     req_data = req.model_dump(exclude_unset=True) # Pydantic.BaseModel -> python.dict
     
     # Request a reply to ai server
@@ -52,6 +61,13 @@ def post_chat(db: SessionDep, req: ChatCreate, current_user: CurrentUser) -> Any
 # Fetch Chats belong to a Chat Session
 @router.get('/{session_id}', response_model=ChatHistoryResponse)
 def fetch_chats(db: SessionDep, session_id: UUID, current_user: CurrentUser) -> Any:
+    """
+    Fetch chats of a session by uuid of the chat session. \n
+    Token Required. \n
+    - **session_id**: uuid of the chat session \n
+    403 Error - Invalid token. \n
+    404 Error - User with the token not found.
+    """
     chat_list = read_chats(db, session_id)
     res_data = ChatHistoryResponse(
         session_id=session_id,
