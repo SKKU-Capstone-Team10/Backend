7a3919ba03410ca63c5d2ce907fc00b7ff62ab0b
ljh4770 <ljh4770@naver.com>
2025-05-20 23:34:04 +0900

    feat(Chat) - Add key - title, ticker

--- api/functions/rag.py
+++ api/functions/rag.py
@@ -6,15 +6,27 @@ from core.config import settings
 
 # Pass the user query to RAG server
 # Get reply from the RAG server
-def get_ai_reply(content: str) -> str:
+def get_ai_reply(content: str, is_first_chat: bool) -> str:
     try:
         response = requests.post(
             f"{settings.RAG_HOST}{settings.RAG_ROUTE}",
-            json={"query": content},
+            json={
+                "query": content,
+                "is_first_chat": is_first_chat
+            },
             timeout=10
         )
         response.raise_for_status()
         result = response.json()
-        return result.get("reply", "No reply from RAG server")
+        title = result.get("title", None)
+        reply = result.get("reply", "No reply from RAG server")
+        ticker = result.get("ticker", "No ticker pased from RAG server")
+        res = {
+            "title": title,
+            "reply": reply,
+            "ticker": ticker
+        }
+        return res
+    
     except requests.RequestException as e:
         raise HTTPException(status_code=500, detail="Exception while requesting RAG server")
