1a3f54a858195ff26e40f4fc884f4cbee13b2a0f
ljh4770 <ljh4770@naver.com>
2025-05-21 01:22:13 +0900

    feat(UUID) - parse string to UUID if not valid raise HTTP 422 Error

--- core/db.py
+++ core/db.py
@@ -1,6 +1,7 @@
 from typing import Annotated
+from uuid import UUID
 from sqlmodel import SQLModel, create_engine, Session
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from core.config import settings
 from models import *
 
@@ -14,4 +15,20 @@ def get_db():
     with Session(engine) as session:
         yield session
 
+def parse_uuid(id: str):
+    try:
+        uid = UUID(id)
+    except ValueError:
+        hex32 = id.strip()
+        if len(hex32) == 32:
+            formatted = f"{hex32[0:8]}-{hex32[8:12]}-{hex32[12:16]}-{hex32[16:20]}-{hex32[20:32]}"
+            try:
+                uid = UUID(formatted)
+            except ValueError:
+                raise HTTPException(status_code=422, detail="Invalid UUID format")
+        else:
+            raise HTTPException(status_code=422, detail="Invalid UUID format")
+    return uid
+
 SessionDep = Annotated[Session, Depends(get_db)]
+UuidDep = Annotated[str, Depends(parse_uuid)]
